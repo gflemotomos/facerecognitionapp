275308f309856aa4365e422d3b707010794e75d0
gflemotomos <iflemotomos@gmail.com>
2020-01-18 15:17:33 +0200

    init project

---
+++ src/component/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Rank = () => {
+    return (
+        <div>
+            <div className={'white f3'}>
+                {'Giannis, your current rank is...'}
+            </div>
+            <div className={'white f3'}>
+                {'#10'}
+            </div>
+        </div>
+    )
+};
+
+export default Rank;
