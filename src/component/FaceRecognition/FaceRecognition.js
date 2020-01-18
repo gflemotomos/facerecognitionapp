32dc9a4a30c62aff808f3cd331916c4a15d22f41
gflemotomos <iflemotomos@gmail.com>
2020-01-18 19:13:53 +0200

    box when face recognized

--- src/component/FaceRecognition/FaceRecognition.js
+++ src/component/FaceRecognition/FaceRecognition.js
@@ -1,15 +1,13 @@
 import React from "react";
-import 'FaceRecognition.css';
+import './FaceRecognition.css';
 
 const FaceRecognition = ({imageUrl, box}) => {
-    return (
-        <div className={'center ma'}>
-            <div className={'absolute mt2'}>
-                <img id={'inputimage'} alt = '' src={imageUrl} width={'500px'} height={'auto'} />
-                <div className={'bounding-box'} style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
-            </div>
+    return <div className={'center ma'}>
+        <div className={'absolute mt2'}>
+            <img id={'inputimage'} alt='' src={imageUrl} width={'500px'} height={'auto'}/>
+            <div className={'bounding-box'} style={{top: box.topRow, right: box.rightCol, left: box.leftCol, bottom: box.bottomRow}}></div>
         </div>
-    );
-}
+    </div>;
+};
 
 export default FaceRecognition;
